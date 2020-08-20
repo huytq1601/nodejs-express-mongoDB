b4439d45a1c14675a7a05348641aa8b189e5f72f
huytq <huytq1601@gmail.com>
2020-08-21 00:54:22 +0700

    #84 simple tour model

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((_) => console.log('DB connect successful'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+console.log(Tour);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('listen from port ' + port);
+  console.log(`listen from port ${port}`);
 });
